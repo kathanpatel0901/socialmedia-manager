aadc09b8e2c8975d9304d1272bdae51d1c4b1f4b
cord4archan <cord4.archan@gmail.com>
2024-02-16 00:57:17 +0530

    templates updated

--- social_manager/settings.py
+++ social_manager/settings.py
@@ -22,84 +22,77 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-49@##0%hf)79-s9k@8qybje)(87!_w7c&l8zdj+bll_$$%y^l)'
+SECRET_KEY = "django-insecure-49@##0%hf)79-s9k@8qybje)(87!_w7c&l8zdj+bll_$$%y^l)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ["127.0.0.1", "13.127.88.229","socialmediamanager.in.net"]
-
+ALLOWED_HOSTS = ["127.0.0.1", "13.127.88.229", "socialmediamanager.in.net"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
-    
-    #forms_app
-    'crispy_forms',
-    'crispy_bootstrap5',
-
-    #created_app
-    'signin',
-
-    #allauth_app
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-
-    #social_media
-    'allauth.socialaccount.providers.google',
-    'allauth.socialaccount.providers.facebook', 
-    'allauth.socialaccount.providers.twitter_oauth2',
-    'allauth.socialaccount.providers.linkedin_oauth2',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
+    # forms_app
+    "crispy_forms",
+    "crispy_bootstrap5",
+    # created_app
+    "signin",
+    # allauth_app
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    # social_media
+    "allauth.socialaccount.providers.google",
+    "allauth.socialaccount.providers.facebook",
+    "allauth.socialaccount.providers.twitter_oauth2",
+    "allauth.socialaccount.providers.linkedin_oauth2",
     "allauth.socialaccount.providers.github",
-   
 ]
 
-SITE_ID = 2 
+SITE_ID = 2
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    #allauth
-    'allauth.account.middleware.AccountMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # allauth
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
-ROOT_URLCONF = 'social_manager.urls'
+ROOT_URLCONF = "social_manager.urls"
 import os
+
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                #allauth
-                'django.template.context_processors.request',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                # allauth
+                "django.template.context_processors.request",
             ],
         },
     },
 ]
 
 
-
-WSGI_APPLICATION = 'social_manager.wsgi.application'
+WSGI_APPLICATION = "social_manager.wsgi.application"
 
 
 # Database
@@ -116,15 +109,15 @@ WSGI_APPLICATION = 'social_manager.wsgi.application'
 
 #     }
 # }
-if config("SERVER_TYPE")=="development":
+if config("SERVER_TYPE") == "development":
     db_name = "db-development.sqlite3"
 else:
     db_name = "db-production.sqlite3"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / db_name,
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / db_name,
     }
 }
 
@@ -134,13 +127,13 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
 ]
 
@@ -148,9 +141,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -160,117 +153,119 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-#STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
-#STATICFILES_DIRS = [
- #   BASE_DIR /'static'
-#]
+# STATICFILES_DIRS = [
+#   BASE_DIR /'static'
+# ]
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
-STATIC_ROOT =  'staticfiles' #os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "staticfiles"  # os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
-    'static'
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/"), "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-#allauth
-AUTHENTICATION_BACKEND =[
-    'django.conntrib.auth.backends.ModelBackend',
-
-    'allauth.account.auth_backends.AuthenticationBackend',
+# allauth
+AUTHENTICATION_BACKEND = [
+    "allauth.account.auth_backends.AuthenticationBackend",
+    "allauth.socialaccount.auth_backends.AuthenticationBackend",
 ]
 
-#client_id = 1026452319339-k4h5fshgoaamj3o4uua9t02ckvl4asfp.apps.googleusercontent.com
+# client_id = 1026452319339-k4h5fshgoaamj3o4uua9t02ckvl4asfp.apps.googleusercontent.com
 
-#client_secrate = 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
+# client_secrate = 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
+
 
 SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        'SCOPE': [
-            'profile',
-            'email',
-        ],
-        'AUTH_PARAMS': {
-            'access_type': 'online',
-        },
-        'OAUTH_PKCE_ENABLED': True,
+    "google": {
+        "SCOPE": ["profile", "email"],
+        "AUTH_PARAMS": {"access_type": "online"},
+        "OAUTH_PKCE_ENABLED": True,
     },
-
-    'facebook': {
-        'METHOD': 'oauth2',  # Set to 'js_sdk' to use the Facebook connect SDK
-        'SDK_URL': '//connect.facebook.net/{locale}/sdk.js',
-        'SCOPE': ['email', 'public_profile'],
-        'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
-        'INIT_PARAMS': {'cookie': True},
-        'FIELDS': [
-            'id',
-            'first_name',
-            'last_name',
-            'middle_name',
-            'name',
-            'name_format',
-            'picture',
-            'short_name'
-        ],
-        'EXCHANGE_TOKEN': True,
-        'LOCALE_FUNC': 'path.to.callable',
-        'VERIFIED_EMAIL': False,
-        'VERSION': 'v13.0',
-        'GRAPH_API_URL': 'https://graph.facebook.com/v13.0',
-    },
-
-       'linkedin': {
-        'SCOPE': [
-            'r_basicprofile',
-            'r_emailaddress'
-        ],
-        'PROFILE_FIELDS': [
-            'id',
-            'first-name',
-            'last-name',
-            'email-address',
-            'picture-url',
-            'public-profile-url',
-        ]
-    },
-
-     'github': {
-        'SCOPE': [
-            'user',
-            'repo',
-            'read:org',
-        ],
-    },
-    
 }
+
+SOCIALACCOUNT_GOOGLE_CLIENT_ID = (
+    "673565393019-u7vqcainccj51dhk35vkcaco707ccl1h.apps.googleusercontent.com"
+)
+SOCIALACCOUNT_GOOGLE_SECRET = "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"
+
+# SOCIALACCOUNT_PROVIDERS = {
+#     'google': {
+#         'SCOPE': ['profile','email'],
+#         'AUTH_PARAMS': {'access_type': 'online'},
+#         'OAUTH_PKCE_ENABLED': True,
+#     },
+# 'facebook': {
+#     'METHOD': 'oauth2',  # Set to 'js_sdk' to use the Facebook connect SDK
+#     'SDK_URL': '//connect.facebook.net/{locale}/sdk.js',
+#     'SCOPE': ['email', 'public_profile'],
+#     'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+#     'INIT_PARAMS': {'cookie': True},
+#     'FIELDS': [
+#         'id',
+#         'first_name',
+#         'last_name',
+#         'middle_name',
+#         'name',
+#         'name_format',
+#         'picture',
+#         'short_name'
+#     ],
+#     'EXCHANGE_TOKEN': True,
+#     'LOCALE_FUNC': 'path.to.callable',
+#     'VERIFIED_EMAIL': False,
+#     'VERSION': 'v13.0',
+#     'GRAPH_API_URL': 'https://graph.facebook.com/v13.0',
+# },
+
+#    'linkedin': {
+#     'SCOPE': [
+#         'r_basicprofile',
+#         'r_emailaddress'
+#     ],
+#     'PROFILE_FIELDS': [
+#         'id',
+#         'first-name',
+#         'last-name',
+#         'email-address',
+#         'picture-url',
+#         'public-profile-url',
+#     ]
+# },
+
+#  'github': {
+#     'SCOPE': [
+#         'user',
+#         'repo',
+#         'read:org',
+#     ],
+# },
+
+# }
 # ACCOUNT_EMAIL_REQUIRED = True
 # ACCOUNT_USERNAME_REQUIRED = False
 # ACCOUNT_AUTHENTICATION_METHOD = 'email'
-ACCOUNT_EMAIL_VERIFICATION = 'none'
-LOGIN_REDIRECT_URL ='/'
-LOGOUT_REDIRECT_URL ='/index'
+# ACCOUNT_EMAIL_VERIFICATION = 'none'
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/index"
 
 # SOCIALACCOUNT_ADAPTER = 'signin.adapters.google_adapter.GoogleSocialAccountAdapter'
 
 # SOCIALACCOUNT_ADAPTER = 'signin.adapters.twitter_adapter.TwitterSocialAccountAdapter'
 
-TWITTER_API_KEY = 'K6j9cjrkcddpNgLZcurk627tE'
-TWITTER_API_SECRET_KEY = '7avCQF3YcfyazKiLMXA7LRKo4nV6P67Syxj0J5BYflzeNcY6iv'
-TWITTER_ACCESS_TOKEN = '1698066183910768640-9RqKMzlmO7muSZ8T7CexRkXGjVC4pu'
-TWITTER_ACCESS_TOKEN_SECRET = '9tQzV0z2dc2vEE7PY9Eiz10m7A1Qt3yayrZZrSVvL7J9m'
-
-
+TWITTER_API_KEY = "K6j9cjrkcddpNgLZcurk627tE"
+TWITTER_API_SECRET_KEY = "7avCQF3YcfyazKiLMXA7LRKo4nV6P67Syxj0J5BYflzeNcY6iv"
+TWITTER_ACCESS_TOKEN = "1698066183910768640-9RqKMzlmO7muSZ8T7CexRkXGjVC4pu"
+TWITTER_ACCESS_TOKEN_SECRET = "9tQzV0z2dc2vEE7PY9Eiz10m7A1Qt3yayrZZrSVvL7J9m"
 
 
 # SECURE_SSL_REDIRECT = True
