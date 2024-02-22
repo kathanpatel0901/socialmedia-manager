ae0a18ec45f0431d3f17a6639a20097bc2815687
cord4archan <cord4.archan@gmail.com>
2024-02-23 01:32:35 +0530

    post tweet main

--- signin/apps.py
+++ signin/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class SigninConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'signin'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "signin"
--- signin/views.py
+++ signin/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import logout
 from django.views import View
@@ -11,43 +11,46 @@ from tweepy.auth import OAuth1UserHandler, OAuth2AppHandler
 from tweepy.client import Client, Response
 
 
-
 def index(request):
-  if request.user.is_authenticated:
-    return render(request,'dashboard/home.html')
-  else:
-      return render(request, 'account/index.html')
+    if request.user.is_authenticated:
+        return render(request, "dashboard/home.html")
+    else:
+        return render(request, "account/index.html")
 
 
 def home(request):
-#    return render(request, "dashboard/home.html")
- return index(request)
-
-
+    #    return render(request, "dashboard/home.html")
+    return index(request)
 
 
 def login(request):
-    return render(request, 'account/login.html')
-    
+    return render(request, "account/login.html")
+
+
 def social_accounts(request):
-    return render(request,'dashboard/social_accounts.html')
+    return render(request, "dashboard/social_accounts.html")
+
 
 def profile_view(request):
-   return render(request, 'dashboard/profile.html')
+    return render(request, "dashboard/profile.html")
 
 
 def google_redirect(request):
-    return render(request, 'dashboard/home.html')
+    return render(request, "dashboard/home.html")
+
 
 def twitter_redirect(request):
-    return render(request, 'account/post.html')
+    return render(request, "account/post.html")
+
 
 # views.py
 def post_tweet(request):
-   return render(request, 'dashboard/post.html' )
+    return render(request, "dashboard/post.html")
+
 
 def post_success(request):
-   return render(request, 'dashboard/post_success.html')
+    return render(request, "dashboard/post_success.html")
+
 
 # @login_required
 # def post_tweet(request):
@@ -71,25 +74,29 @@ def post_success(request):
 #     return render(request, 'dashboard/post.html', {'form': form})
 
 
-
 def tweet(request):
-    BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAB6AsQEAAAAAKUpf0jySlMJCNFSb%2B73NY%2FwKPyc%3DGRBoKmU6Y8KIsLiRrNboMNqAJ7flBFHuLrNsitBu61G4c4E4dP'
-    CONSUMER_KEY = 'Y2hCQXhVVzl0Y0RMWmR2T19XZ3Y6MTpjaQ'
-    CONSUMER_SECRATE ='L4Q4EsxJLc5Byw3428sRLbxIVMua8ZdqVDiycUYpB9HpuTKAqM'
-    TWITTER_API_KEY = '2PegHCEvjFbsF7mLgJbxQQzks'
-    TWITTER_API_SECRET_KEY = 'XsTi5z1MmOZ9tUu8r5yituADNvrnFQmIbzsnqmgUUPfRKwc2hI'
-    TWITTER_ACCESS_TOKEN = '1698066183910768640-qVGbLnFiIwvJ7uWKWcXYYp1yjOOzyF'
-    TWITTER_ACCESS_TOKEN_SECRET = 'ycObapiXdEf91PQBye23LuoSb1IGDEqAzdqWKQlpxfjyi'
+    twitter_auth_keys = {
+        "consumer_key": "ULYeOm844iifGFuE32YyLnzT0",
+        "consumer_secret": "xQ9YJjXLDmIjf67e6ZGrxxrhZxg4pyFlnV6qbLYEMEfrbavxDb",
+        "access_token": "1758379615968514048-7fCJHGXHwU52KMAS2p1HMOQCjlIymx",
+        "access_token_secret": "95eCaWA79LD48G8PgT2pzFAb6CGIqURA9hXm6y1OrUmvJ",
+    }
 
-    if request.method == 'POST':
-        content = request.POST.get('content')
+    if request.method == "POST":
+        content = request.POST.get("content")
 
-        if content:        
-            auth = OAuth2AppHandler(CONSUMER_KEY,CONSUMER_SECRATE)
-            api = Client(auth)
-            test = api.create_tweet(text=content)
-            return render(request,'dashboard/post_success.html')
-    return render(request, 'dashboard/tweet.html')
+        if content:
+            print("authentication")
+            client = tweepy.Client(
+                consumer_key=twitter_auth_keys["consumer_key"],
+                consumer_secret=twitter_auth_keys["consumer_secret"],
+                access_token=twitter_auth_keys["access_token"],
+                access_token_secret=twitter_auth_keys["access_token_secret"],
+            )
+            print("postig tweet")
+            client.create_tweet(text=content)
+            print("==================")
 
+            return render(request, "dashboard/post_success.html")
 
-
+    return render(request, "dashboard/tweet.html")
